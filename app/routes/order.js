c1bb03079efa9d16759b1d1f7b4f1df064f0043c
Youpi <41284472+Chickyy@users.noreply.github.com>
2022-03-25 16:30:36 +0100

    fin séance (début partie B)

--- app/routes/order.js
+++ app/routes/order.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import Abstractroute from "./AbstractRoute";
 
-export default class OrderRoute extends Route {}
+export default class OrderRoute extends Abstractroute {
+  model(params) {
+    return this.store.findRecord('order', params.order_id, {
+      include: 'orderdetails.product, user',
+    });
+  }
+}
